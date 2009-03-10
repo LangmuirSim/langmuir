eaa085e55b88e133e53bb585ddc6e480066f639c
Marcus D. Hanwell <marcus@cryos.org>
2009-03-10 17:33:20 -0400

    Forgot to save...

--- world.h
+++ world.h
@@ -1,4 +1,21 @@
+/**
+ * World - acts as a container for global information in the simulation
+ */
+
 #ifndef WORLD_H
 #define WORLD_H
+
+namespace Langmuir
+{
+
+  class World
+  {
+  public:
+    World();
+    ~World();
+
+
+
+  };
 
 #endif // WORLD_H
