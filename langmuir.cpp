38791a79b834883d1656becb17d4a80524ff32d5
Adam Gagorik <agg7@pitt.edu>
2012-03-29 10:58:42 -0400

    modified output file extensions

--- langmuir.cpp
+++ langmuir.cpp
@@ -106,7 +106,7 @@ int main (int argc, char *argv[])
     // Output time
     if (par.outputIsOn)
     {
-        OutputStream timerStream("%path/time.dat",&par);
+        OutputStream timerStream("%path/%stub.time",&par);
 
         timerStream << right
                     << qSetFieldWidth(20)
