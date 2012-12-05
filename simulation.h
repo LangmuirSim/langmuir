198f8c1f5d8a95019f3c795d862a355a419eaa30
Adam Gagorik <adam@adam-work>
2012-12-05 10:01:43 -0500

    Added comments to simulation class.

--- simulation.h
+++ simulation.h
@@ -15,6 +15,9 @@ class SourceAgent;
 class ChargeAgent;
 struct SimulationParameters;
 
+/**
+ * @brief A class to orchestrate the calculation
+ */
 class Simulation : public QObject
 {
 private:
@@ -22,17 +25,62 @@ private:
     Q_DISABLE_COPY(Simulation)
 
 public:
+    /**
+     * @brief Create a Simulation
+     * @param world reference to World Object
+     * @param parent QObject this belongs to
+     */
     Simulation(World &world, QObject *parent = 0);
+
+    /**
+     * @brief Destroy the Simulation
+     */
     virtual ~Simulation();
+
+    /**
+     * @brief simulate for a set number of steps
+     * @param nIterations the number of steps to simulate
+     */
     virtual void performIterations(int nIterations);
-    void equilibrated();
 
 protected:
+
+    /**
+     * @brief Recombine holes and electrons (in solarcell simulations only)
+     */
     void performRecombinations();
+
+    /**
+     * @brief Tell all sources to inject charges
+     */
     void performInjections();
+
+    /**
+     * @brief Remove charges from the simulation
+     *
+     * Charges are removed only if a DrainAgent sets their removed status to True.
+     * This function will also output carrier statistics if output.id.on.delete
+     * is set.
+     */
     void nextTick();
+
+    /**
+     * @brief A method needed to call ChargeAgent::coulombCPU() in parallel
+     */
     static void chargeAgentCoulombInteractionQtConcurrentCPU(ChargeAgent * chargeAgent);
+
+    /**
+     * @brief A method needed to call ChargeAgent::coulombGPU() in parallel
+     *
+     * Does not perform GPU calcuations.  The coulomb kernel in OpenCLHelper is used to do that.
+     * This function copies the GPU results from OpenCLHelper to each ChargeAgent. It is assumed
+     * that the coulomb kernel was launched beforehand.
+     */
     static void chargeAgentCoulombInteractionQtConcurrentGPU(ChargeAgent * chargeAgent);
+
+    /**
+     * @brief Reference to World object
+     */
     World &m_world;
 };
 
