fa971875327fd2364dc31d7adda040fe2d968795
Adam Gagorik <adam.gagorik@gmail.com>
2014-02-19 13:34:44 -0500

    Fixed setup script.

--- LangmuirPython/setup.py
+++ LangmuirPython/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name         = "Langmuir Python",
-      version      = 2.0,
+      version      = '2.0',
       description  = "Langmuir Python",
       author       = "Adam Gagorik",
       author_email = "adam.gagorik@gmail.com",
