02ad7da9a5edd0f2fc8ac1ee2b940bba8ce5be88
Adam Gagorik <adam.gagorik@gmail.com>
2014-02-28 15:48:15 -0500

    Updated find docs.

--- LangmuirPython/langmuir/find.py
+++ LangmuirPython/langmuir/find.py
@@ -13,15 +13,33 @@ def find(work, r=False, single=True, absolute=True, stub='*', ext=None,
     """
     A method for searching for files and directories using patterns.
 
-    single        : do not return a list
-    recursive     : perform the search recursivly
-    absolute      : return absolute paths
-    stub          : the wildcard-able search pattern (`*` is the wildcard)
-    exclude_dirs  : do not include directories in the search
-    exclude_files : do not include files in the search
-    sort_by       : a function (applied to paths) used to sort the results
-    at_least_one  : make sure at least one thing was found
-    follow_links  : do not follow symbolic links
+    :param work: path to search
+    :param r: perform the search recursivly
+    :param single: do not return a list
+    :param absolute: return absolute paths
+    :param stub: the wildcard-able search pattern (star is the wildcard)
+    :param ext: the filename extension
+    :param exclude_dirs: do not include directories in the search
+    :param exclude_files: do not include files in the search
+    :param sort_by: a function (applied to paths) used to sort the results
+    :param at_least_one: make sure at least one thing was found
+    :param follow_links: do not follow symbolic links
+
+    :type work: str
+    :type r: bool
+    :type single: bool
+    :type absolute: bool
+    :type stub: str
+    :type ext: str
+    :type exclude_dirs: bool
+    :type exclude_files: bool
+    :type sort_by: func
+    :type at_least_one: bool
+    :type follow_links: bool
+
+    >>> import os
+    >>> work = os.getcwd()
+    >>> pkls = find(work, ext='txt*') # find all txt files in work
     """
     work = os.path.expanduser(work)
 
@@ -70,6 +88,16 @@ def find(work, r=False, single=True, absolute=True, stub='*', ext=None,
 def systems(work, **kwargs):
     """
     Search for directories that contain "runs".
+
+    :param args: see :func:`runs`
+    :param kwargs: see :func:`runs`
+
+    >>> systems = lm.find.systems(r'/home/adam/simulations')
+    >>> print '\\n'.join(systems)
+    '/home/adam/simulations/systemA'
+    '/home/adam/simulations/systemB'
+    '/home/adam/simulations/systemC'
+    '/home/adam/simulations/systemD'
     """
     kwargs.update(r=True)
     work  = os.path.expanduser(work)
@@ -84,6 +112,17 @@ def systems(work, **kwargs):
 def sims(work, **kwargs):
     """
     Search for directories that contain "parts".
+
+    :param args: see :func:`parts`
+    :param kwargs: see :func:`parts`
+
+    >>> work = r'/home/adam/simulations/systemA/run.0'
+    >>> sims = lm.find.sims(work, stub='voltage.right')
+    >>> print '\\n'.join(systems)
+    '/home/adam/simulations/systemA/run.0/voltage.right_+0.0'
+    '/home/adam/simulations/systemA/run.0/voltage.right_+0.2'
+    '/home/adam/simulations/systemA/run.0/voltage.right_+0.4'
+    '/home/adam/simulations/systemA/run.0/voltage.right_+0.8'
     """
     kwargs.update(r=True)
     work  = os.path.expanduser(work)
@@ -96,98 +135,269 @@ def sims(work, **kwargs):
     return found
 
 def runs(*args, **kwargs):
+    """
+    Search for directories of the form run*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+
+    >>> runs = lm.find.runs(r'/home/adam/simulations/systemA/')
+    >>> print '\\n'.join(runs)
+    '/home/adam/simulations/systemA/run.0/'
+    '/home/adam/simulations/systemA/run.1/'
+    '/home/adam/simulations/systemA/run.2/'
+    '/home/adam/simulations/systemA/run.3/'
+    """
     _kwargs = dict(stub='run*', exclude_files=True, single=False)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def run(*args, **kwargs):
+    """
+    Search for a single directory of the form run*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+
+    >>> run = lm.find.run(r'/home/adam/simulations/systemA')
+    RuntimeError: found multiple run* in directory:
+        /home/adam/simulations/systemA
+            run.0
+            run.1
+            run.2
+            run.3
+    """
     _kwargs = dict(stub='run*', exclude_files=True, single=True)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def parts(*args, **kwargs):
+    """
+    Search for directories of the form part*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+
+    >>> prts = lm.find.parts(r'~/simulations/systemA/run.0/voltage.right_+0.0')
+    >>> print '\\n'.join(prts)
+    '/home/adam/simulations/systemA/run.0/voltage.right_+0.0/part.0/'
+    '/home/adam/simulations/systemA/run.0/voltage.right_+0.0/part.1/'
+    """
     _kwargs = dict(stub='part*', exclude_files=True, single=False)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def part(*args, **kwargs):
+    """
+    Search for a single directory of the form part*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+
+    >>> part = lm.find.parts(r'~/simulations/systemA/run.0/voltage.right_+0.0')
+    RuntimeError: found multiple part* in directory:
+        /home/adam/simulations/systemA/run.0/voltage.right_+0.0
+            part.0
+            part.1
+    """
     _kwargs = dict(stub='part*', exclude_files=True, single=True)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def inps(*args, **kwargs):
+    """
+    Search for files of the form *.inp*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> inps = lm.find.inps('.')
+    """
     _kwargs = dict(ext='inp*', exclude_dirs=True, single=False)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def inp(*args, **kwargs):
+    """
+    Search for a single file of the form *.inp*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> inp = lm.find.inp('.')
+    """
     _kwargs = dict(ext='inp*', exclude_dirs=True, single=True)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def chks(*args, **kwargs):
+    """
+    Search for files of the form *.chk*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> chks = lm.find.chks('.')
+    """
     _kwargs = dict(ext='chk*', exclude_dirs=True, single=False)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def chk(*args, **kwargs):
+    """
+    Search for a single file of the form *.chk*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> chk = lm.find.chk('.')
+    """
     _kwargs = dict(ext='chk*', exclude_dirs=True, single=True)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def parms(*args, **kwargs):
+    """
+    Search for files of the form *.parm*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> parms = lm.find.parms('.')
+    """
     _kwargs = dict(ext='parm*', exclude_dirs=True, single=False)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def parm(*args, **kwargs):
+    """
+    Search for a single file of the form *.parm*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> parm = lm.find.parm('.')
+    """
     _kwargs = dict(ext='parm*', exclude_dirs=True, single=True)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def dats(*args, **kwargs):
+    """
+    Search for files of the form *.dat*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> dats = lm.find.dats('.')
+    """
     _kwargs = dict(ext='dat*', exclude_dirs=True, single=False)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def dat(*args, **kwargs):
+    """
+    Search for a single file of the form *.dat*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> dat = lm.find.dat('.')
+    """
     _kwargs = dict(ext='dat*', exclude_dirs=True, single=True)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def txts(*args, **kwargs):
+    """
+    Search for files of the form *.txt*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> txts = lm.find.txts('.')
+    """
     _kwargs = dict(ext='txt*', exclude_dirs=True, single=False)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def txt(*args, **kwargs):
+    """
+    Search for a single file of the form *.txt*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> txt = lm.find.txt('.')
+    """
     _kwargs = dict(ext='txt*', exclude_dirs=True, single=True)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def pkls(*args, **kwargs):
+    """
+    Search for files of the form *.pkl*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> pkls = lm.find.pkls('.')
+    """
     _kwargs = dict(ext='pkl*', exclude_dirs=True, single=False)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def pkl(*args, **kwargs):
+    """
+    Search for a single file of the form *.pkl*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> pkl = lm.find.pkl('.')
+    """
     _kwargs = dict(ext='pkl*', exclude_dirs=True, single=True)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def pngs(*args, **kwargs):
+    """
+    Search for files of the form *.png*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> pngs = lm.find.pngs('.')
+    """
     _kwargs = dict(ext='png*', exclude_dirs=True, single=False)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def png(*args, **kwargs):
+    """
+    Search for a single file of the form *.png*.
+
+    :param args: see :func:`find`
+    :param kwargs: see :func:`find`
+    
+    >>> png = lm.find.png('.')
+    """
     _kwargs = dict(ext='png*', exclude_dirs=True, single=True)
     _kwargs.update(**kwargs)
     return find(*args, **_kwargs)
 
 def slice_path(path, regex):
     """
-    Return dirname of path where the regex matches
+    Return dirname of path where the regex matches.
+
+    :param path: path to parse
+    :param regex: regex to match
+
+    :type path: str
+    :type regex: str
+
+    >>> print slice_path('r/home/adam/Desktop', 'adam')
+    '/home/adam'
     """
     if isinstance(regex, str):
         regex = re.compile(regex)
@@ -201,22 +411,40 @@ def slice_path(path, regex):
             break
     raise RuntimeError('can not slice path on run')
 
-def slice_part(path, regex='part'):
+def slice_part(path):
     """
     Return dirname of path where run directory is.
+
+    :param path: path to parse
+    :type path: str
+
+    >>> print slice_path('r/system/run/sim/part')
+    '/system/run/sim/part'
     """
-    return slice_path(path, regex)
+    return slice_path(path, 'part')
 
 
-def slice_sim(path, regex='sim'):
+def slice_sim(path):
+    """
+    Return dirname of path where sim directory is.
+
+    :param path: path to parse
+    :type path: str
+
+    >>> print slice_path('r/system/run/sim/part')
+    '/system/run/sim'
+    """
+    return slice_path(path, 'sim')
+
+
+def slice_run(path):
     """
     Return dirname of path where run directory is.
-    """
-    return slice_path(path, regex)
 
+    :param path: path to parse
+    :type path: str
 
-def slice_run(path, regex='run'):
+    >>> print slice_path('r/system/run/sim/part')
+    '/system/run'
     """
-    Return dirname of path where run directory is.
-    """
-    return slice_path(path, regex)
+    return slice_path(path, 'run')
