a5bebf1ed979344a0b3979647db05ff5031701f7
Adam Gagorik <adam.gagorik@gmail.com>
2014-02-28 15:48:35 -0500

    Fixed number func and added regex docs.

--- LangmuirPython/langmuir/regex.py
+++ LangmuirPython/langmuir/regex.py
@@ -2,6 +2,7 @@
 """
 @author: adam
 """
+import langmuir as lm
 import re
 
 regex_comments = re.compile(r'\s*#.*$')
@@ -9,7 +10,7 @@ regex_false    = re.compile(r'false', re.IGNORECASE)
 regex_true     = re.compile(r'true', re.IGNORECASE)
 regex_number   = re.compile(r'((?:[-\+]?\d+\.?\d*)(?:[eE](?:[-\+])?\d+)?)')
 
-def number(string, index=1, pytype=float):
+def number(string, index=0, pytype=float):
     """
     Get numbers in a string at index and convert them.
 
@@ -20,8 +21,11 @@ def number(string, index=1, pytype=float):
     :type string: str
     :type index: int
     :type pytype: type
+
+    >>> print lm.regex.number('asdfasdfa1.231e10')
+    12310000000.0
     """
-    return pytype(regex_number.match(string).group(index))
+    return lm.regex.numbers(string, pytype)[index]
 
 def numbers(string, pytype=float):
     """
@@ -34,6 +38,9 @@ def numbers(string, pytype=float):
     :type string: str
     :type index: int
     :type pytype: type
+    
+    >>> print lm.regex.numbers('asdfasdfa1.231e10asdf1209asd asd0912 sdaf9 81')
+    [12310000000.0, 1209.0, 912.0, 9.0, 81.0]
     """
     return [pytype(f) for f in regex_number.findall(string)]
 
@@ -43,6 +50,9 @@ def strip_comments(string):
 
     :param string: string
     :type string: str
+    
+    >>> print lm.regex.strip_comments('hello # goodbye')
+    hello
     """
     return regex_comments.sub('', string)
 
@@ -52,6 +62,9 @@ def fix_boolean(string):
 
     :param string: string
     :type string: str
+    
+    >>> print lm.regex.fix_boolean('true false True False true false')
+    True false True False True false
     """
     fixed = regex_false.sub('False', string)
     fixed = regex_true.sub('True', string)
