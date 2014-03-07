dd52e9d47399a74f5b64297f0b8d1de9a9e8e4ca
Adam Gagorik <adam.gagorik@gmail.com>
2014-03-07 09:32:50 -0500

    Added sphinx ext for mpl plots.

--- LangmuirPython/doc/source/conf.py
+++ LangmuirPython/doc/source/conf.py
@@ -25,7 +25,16 @@ sys.path.insert(0, os.path.abspath('../../langmuir'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.pngmath',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'matplotlib.sphinxext.plot_directive'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
