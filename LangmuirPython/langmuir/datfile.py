be0d486433526d78fc907a97cc8ef08dba84eb0c
Adam Gagorik <adam.gagorik@gmail.com>
2014-03-14 09:22:17 -0400

    fixed encoding

--- LangmuirPython/langmuir/datfile.py
+++ LangmuirPython/langmuir/datfile.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 .. note::
     Functions for opening Langmuir output files.
--- LangmuirPython/langmuir/vtkutils.py
+++ LangmuirPython/langmuir/vtkutils.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 .. note::
     Functions for working with vtk
