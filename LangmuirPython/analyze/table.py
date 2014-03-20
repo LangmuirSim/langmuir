71cd9055256c31fca139d29a2f4d28bb35c377c0
Adam Gagorik <adam.gagorik@gmail.com>
2014-03-20 14:27:52 -0400

    Cleaned up table.

--- LangmuirPython/analyze/table.py
+++ LangmuirPython/analyze/table.py
@@ -30,24 +30,65 @@ def get_arguments(args=None):
     opts = parser.parse_args(args)
     return opts
 
+extract = {}
+extract['system'] = lambda pkl, data: os.path.relpath(lm.find.slice_system(pkl), work)
+extract['run'   ] = lambda pkl, data: lm.regex.run(pkl)
+
+columns = [
+    'system',
+    'run',
+    'v_oc',
+    'i_sc',
+    'p_th',
+    'j_sc',
+    'r_th',
+    'i_mp',
+    'v_mp',
+    'p_mp',
+    'j_mp',
+    'r_mp',
+    'fill',
+]
+
+def sort_table(table):
+    table = table.set_index('system')
+
+    def sort_key(path):
+        items = []
+        for item in lm.regex.regex_number.split(path):
+            try:
+                items.append(float(item))
+            except ValueError:
+                items.extend(item.split(os.sep))
+        return filter(None, items)
+
+    table = table.reindex_axis(sorted(table.index, key=sort_key), axis=0)
+
+    table['system'] = table.index
+    table = table.reindex_axis(columns, axis=1)
+
+    table = table.reset_index(drop=True)
+
+    return table
+
 if __name__ == '__main__':
     work = os.getcwd()
     opts = get_arguments()
     pkls = lm.find.pkls(work, stub='solar*', r=True)
 
     table = collections.OrderedDict()
-    table['system'] = []
-    table['run'] = []
+    for column in columns:
+        table[column] = []
 
     for pkl in pkls:
-        table['system'].append(lm.find.extract_system(pkl))
-        table['run'].append(lm.regex.run(pkl))
         data = lm.common.load_pkl(pkl)
-        for key, value in data.iteritems():
-            if not hasattr(value, '__len__'):
-                if not key in table:
-                    table[key] = []
-                table[key].append(value)
+        for c in columns:
+            if c in data:
+                table[c].append(data[c])
+            else:
+                table[c].append(extract[c](pkl, data))
 
     table = pd.DataFrame(table)
-    table.to_csv('table.csv')
+    table = sort_table(table)
+
+    table.to_csv('table.csv', index=False)
