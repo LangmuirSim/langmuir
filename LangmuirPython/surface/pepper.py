721481188d3e534fa26cf316d26d45d7a342d283
Adam Gagorik <adam.gagorik@gmail.com>
2014-03-20 11:43:44 -0400

    Implemented pepper script.

--- LangmuirPython/surface/pepper.py
+++ LangmuirPython/surface/pepper.py
@@ -11,6 +11,7 @@ pepper.py
 .. moduleauthor:: Adam Gagorik <adam.gagorik@gmail.com>
 """
 import langmuir as lm
+import numpy as np
 import collections
 import argparse
 import sys
@@ -20,21 +21,28 @@ desc = """
 Add pepper to phase(s).
 """
 
+brushes = {}
+brushes['point' ] = np.array([[0, 0, 0]])
+brushes['square'] = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0]])
+brushes['cross' ] = np.array([[ 0,  0,  0], [ 0,  0, -1], [0, 0, 1], [-1, 0, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0]])
+
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.description = desc
 
-    parser.add_argument(dest='ifile', type=str, metavar='input',
-        help='input file')
-    parser.add_argument(dest='ofile', type=str, metavar='output', default=None, nargs='?',
-        help='output file')
+    parser.add_argument(dest='ifile', type=str, metavar='input', help='input file')
+    parser.add_argument(dest='ofile', type=str, metavar='output', help='output file')
 
-    parser.add_argument('--percent', type=float, metavar='float', default=0.0,
-        help='pepper conecntration')
-    parser.add_argument('--size', type=int, metavar='int', default=1,
-        help='pixel size')
-    parser.add_argument('--phase', type=str, metavar='str', default='both', choices=['both', '0', '1'],
-        help='which phase to add pepper to')
+    groupA = parser.add_mutually_exclusive_group(required=True)
+    groupA.add_argument('--count', type=int, metavar='int', default=None, help='number of strokes')
+    groupA.add_argument('--percent', type=float, metavar='float', default=None,
+        help='calculate number of strokes based on percent of phase volume')
+
+    parser.add_argument('--brush', type=str, metavar='str', default='point', choices=brushes.keys(),
+        help='pepper brush type')
+    parser.add_argument('--phase', type=int, metavar='int', default=0, help='phase ID')
+
+    parser.add_argument('--show', action='store_true', help='show plot')
 
     return parser
 
@@ -43,8 +51,97 @@ def get_arguments(args=None):
     opts = parser.parse_args(args)
     return opts
 
+def info(image):
+    print '[Image]'
+    print '  {0:10}: {1}, {2}, {3}'.format('shape', *image.shape)
+    print '  {0:10}: {1}'.format('size', image.size)
+    phases = np.unique(image)
+    for i, phase in enumerate(phases):
+        p = image[image==phases[i]]
+        percent = p.size/float(image.size) * 100
+        print '[Phase {0}]'.format(i)
+        print '  {0:10}: {1}'.format('size', p.size)
+        print '  {0:10}: {1:.3f}'.format('percent', percent)
+    print ''
+
 if __name__ == '__main__':
     work = os.getcwd()
     opts = get_arguments()
 
-    print opts
+    image = lm.surface.load(opts.ifile)
+
+    phases = np.unique(image)
+
+    if not phases.size == 2:
+        raise RuntimeError('invalid phase count: %s' % phases.size)
+
+    if not (0 <= opts.phase < phases.size):
+        raise RuntimeError('invalid phase ID: %d' % opts.phase)
+
+    info(image)
+
+    x_i, y_i, z_i = np.where(image==phases[opts.phase])
+
+    size = x_i.size
+    if opts.count is None:
+        opts.count = int(opts.percent * size)
+
+    print 'to_change: %d\n' % opts.count
+
+    pixels_changed = 0
+    while pixels_changed < opts.count:
+        i = np.random.randint(0, x_i.size - 1)
+        to_paint = brushes[opts.brush] + (x_i[i], y_i[i], z_i[i])
+
+        x_j, y_j, z_j = to_paint[:,0], to_paint[:,1], to_paint[:,2]
+
+        valid = ((x_j < image.shape[0]) & (x_j >= 0) &
+                 (y_j < image.shape[0]) & (y_j >= 0) &
+                 (z_j < image.shape[0]) & (z_j >= 0))
+
+        x_j = x_j[valid]
+        y_j = y_j[valid]
+        z_j = z_j[valid]
+
+        valid = (image[x_j, y_j, z_j] == phases[opts.phase])
+        x_j = x_j[valid]
+        y_j = y_j[valid]
+        z_j = z_j[valid]
+
+        if x_j.size == brushes[opts.brush].shape[0]:
+            image[x_j, y_j, z_j] = phases[opts.phase - 1]
+
+        x_i, y_i, z_i = np.where(image==phases[opts.phase])
+
+        pixels_changed += x_j.size
+
+    print '[Changed]'
+    print '  {0:10}: {1}'.format('pixels', pixels_changed)
+    print '  {0:10}: {1:.3f}'.format('percent', pixels_changed/float(size) * 100)
+    print ''
+
+    info(image)
+
+    stub, ext = lm.common.splitext(opts.ofile)
+    handle = lm.common.format_output(stub=stub, name='pepper', ext=ext)
+    print 'saved: %s' % handle
+    lm.surface.save(handle, image)
+
+    if opts.show:
+        grid = lm.grid.Grid(*image.shape)
+        if not image.shape.count(1):
+            try:
+                print 'importing mayavi...'
+                import mayavi.mlab as mlab
+                mlab.contour3d(grid.mx, grid.my, grid.mz, image, contours=3)
+                mlab.show()
+                sys.exit(-1)
+            except ImportError:
+                if not lm.vtk is None:
+                    lm.vtkutils.show(lm.vtkutils.filter_marching(lm.vtkutils.create_image_data_from_array(image)))
+                    sys.exit(-1)
+                else:
+                    print 'can not create 3D plot'
+
+        lm.plot.contourf(grid.mx, grid.my, image, 3)
+        lm.plot.plt.show()
