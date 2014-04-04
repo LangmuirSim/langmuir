5b97ceae0a1b67ef07a91d80231f67e2ea277ac6
Adam Gagorik <adam.gagorik@gmail.com>
2014-04-04 11:05:14 -0400

    updated done

--- LangmuirPython/utils/done.py
+++ LangmuirPython/utils/done.py
@@ -11,6 +11,7 @@ done.py
 .. moduleauthor:: Adam Gagorik <adam.gagorik@gmail.com>
 """
 import langmuir as lm
+from langmuir.common import ccodes
 import argparse
 import sys
 import os
@@ -23,6 +24,7 @@ def create_parser():
     parser = argparse.ArgumentParser()
     parser.description = desc
     parser.add_argument('-r', action='store_true', help='search recursivly')
+    parser.add_argument('-d', action='store_true', help='show which simulations are not done')
     return parser
 
 def get_arguments(args=None):
@@ -31,55 +33,103 @@ def get_arguments(args=None):
     return opts
 
 def indent(string, level=0, **kwargs):
-    print '    ' * level + string.format(**kwargs)
+    print '    ' * level + string.format(ccodes=ccodes, **kwargs)
 
-def report_chk(chk):
-    chk = lm.checkpoint.load(chk)
+format_string = '(' + ccodes.f('path'   ) + '='  + ccodes.y('{path}'   ) + ') : ' + \
+                '(' + ccodes.f('cstep'  ) + '='  + ccodes.c('{cstep}'  ) + ') : ' + \
+                '(' + ccodes.f('tstep'  ) + '='  + ccodes.c('{tstep}'  ) + ') : ' + \
+                '(' + ccodes.f('percent') + '='  + ccodes.c('{percent}') + ') : ' + \
+                '(' + ccodes.f('done'   ) + '='  +          '{done}'     + ')'
+
+def get_cstep(obj):
     try:
-        cstep = chk['current.step']
-    except KeyError:
-        cstep = '?'
+        cstep = obj['current.step']
+    except:
+        cstep = None
+    return cstep
 
+def get_tstep(obj):
     try:
-        tstep = chk['iterations.real']
-    except KeyError:
-        tstep = '?'
+        tstep = obj['iterations.real']
+    except:
+        tstep = None
+    return tstep
 
+def get_percent(cstep, tstep):
     try:
         percent = float(cstep)/float(tstep) * 100
-    except TypeError:
-        percent = 0.00
+    except:
+        percent = 0
+    return percent, percent >= 100
 
-    done = False
-    if percent >= 100:
-        done=True
+def analyze_chk(chk):
+    if chk:
+        chk = lm.checkpoint.load(chk)
 
-    percent = '%.2f' % percent
+    cstep = get_cstep(chk)
+    tstep = get_tstep(chk)
 
-    return cstep, tstep, percent, done
+    return cstep, tstep
 
-format_string = '{path} : cstep={cstep:>8} : tstep={tstep:>8} : percent={percent:>5} : done={done}'
+def analyze_dat(dat):
+    try:
+        return int(lm.common.tail(dat).strip().split()[0])
+    except:
+        return None
 
-def loop_part(part, level=0):
+def loop_part(part, level=0, record=True):
     chk = lm.find.chk(part)
-    cstep, tstep, percent, done = report_chk(chk)
-    indent(format_string, level=level, path=os.path.basename(part), cstep=cstep, tstep=tstep, percent=percent,
-           done=done)
+    cstep, tstep = analyze_chk(chk)
+
+    if not tstep:
+        inp = lm.find.inp(part)
+        tstep = get_tstep(inp)
+
+    if not tstep:
+        parm = lm.find.parm(part)
+        tstep = get_tstep(parm)
+
+    if not cstep:
+        dat = lm.find.dat(part)
+        cstep = analyze_dat(dat)
+
+    percent, done = get_percent(cstep, tstep)
+
+    if not tstep:
+        tstep = '?'
+
+    if not cstep:
+        cstep = '?'
+
+    if not done:
+        if record:
+            results['not_done'].append(part)
+        done = ccodes.red('%s' % done)
+    else:
+        if record:
+            results['done'].append(part)
+        done = ccodes.green('%s' % done)
+
+    indent(format_string, level=level, path=os.path.basename(part), cstep=cstep,
+           tstep=tstep, percent=percent, done=done)
 
 def loop_sim(sim, level=0):
-    indent('sim: {sim}', level=level, sim=os.path.basename(sim))
+    title = ccodes.f('sim') + '=' + ccodes.blue('{sim}')
+    indent(title, level=level, sim=os.path.basename(sim))
     parts = lm.find.parts(sim)
     for part in parts:
         loop_part(part, level=level + 1)
 
 def loop_run(run, level=0):
-    indent('run: {run}', level=level, run=os.path.basename(run))
+    title = ccodes.f('run') + '=' + ccodes.magenta('{run}')
+    indent(title, level=level, run=os.path.basename(run))
     sims = lm.find.sims(run)
     for sim in sims:
         loop_sim(sim, level=level + 1)
 
 def loop_system(system, level=0):
-    indent('system: {system}', level=level, system=os.path.basename(system))
+    title = ccodes.f('system') + '=' + ccodes.cyan('{system}')
+    indent(title, level=level, system=os.path.basename(system))
     runs = lm.find.runs(system)
     for run in runs:
         loop_run(run, level=level + 1)
@@ -87,6 +137,11 @@ def loop_system(system, level=0):
 if __name__ == '__main__':
     work = os.getcwd()
     opts = get_arguments()
+
+    results = {
+            'done' : [],
+        'not_done' : []
+    }
 
     systems = lm.find.systems(work, r=opts.r)
     if not systems:
@@ -100,13 +155,19 @@ if __name__ == '__main__':
                     if not chks:
                         dats = lm.find.dats(work, r=opts.r)
                         if not dats:
-                            print 'can not find any simulations!'
-                            sys.exit(-1)
+                            inps = lm.find.inps(work, r=opts.r)
+                            if not inps:
+                                for inp in inps:
+                                    loop_part(os.path.dirname(inp))
+                            else:
+                                print 'can not find any simulations!'
+                                sys.exit(-1)
+                        else:
+                            for dat in dats:
+                                loop_part(os.path.dirname(dat))
                     else:
                         for chk in chks:
-                            cstep, tstep, percent, done = report_chk(chk)
-                            indent(format_string, level=0, path=os.path.basename(chk), cstep=cstep,
-                                tstep=tstep, percent=percent, done=done)
+                            loop_part(os.path.dirname(chk))
                 else:
                     for part in parts:
                         loop_part(part)
@@ -119,3 +180,12 @@ if __name__ == '__main__':
     else:
         for system in systems:
             loop_system(system)
+
+    if opts.d:
+        print ''
+        for not_done in results['not_done']:
+            loop_part(not_done, record=False)
+
+    print ''
+    print '(' + ccodes.f('done'    ) + '=' + ccodes.g('%d' % len(results['done'    ])) + ')' + ' : '  + \
+          '(' + ccodes.f('not done') + '=' + ccodes.r('%d' % len(results['not_done'])) + ')'
