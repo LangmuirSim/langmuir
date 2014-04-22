273df7170efd0a2469ddcb456bb81b69173bd633
Adam Gagorik <adam.gagorik@gmail.com>
2014-04-22 13:52:45 -0400

    corner axis.

--- src/langmuirView/include/langmuirviewer.h
+++ src/langmuirView/include/langmuirviewer.h
@@ -8,12 +8,24 @@ class LangmuirViewer : public QGLViewer
     Q_OBJECT
 public:
     explicit LangmuirViewer(QWidget *parent = 0);
+    bool cornerAxisIsDrawn();
+
+signals:
+    void cornerAxisIsDrawnChanged(bool drawn);
+
+public slots:
+    void toggleCornerAxisIsDrawn();
+    void setCornerAxisIsDrawn(bool draw = true);
 
 protected:
     virtual void draw();
+    virtual void postDraw();
     virtual void init();
     virtual void animate();
     virtual QString helpString() const;
+
+    bool cornerAxisIsDrawn_;
+    virtual void drawCornerAxis();
 };
 
 #endif // LANGMUIRVIEWER_H
