5a08971177769fc41d7e19314bf4250ce55e08e8
Adam Gagorik <adam.gagorik@gmail.com>
2014-05-21 15:29:23 -0400

    Added cube shader.

--- src/langmuirView/shaders/ccGeom.glsl
+++ src/langmuirView/shaders/ccGeom.glsl
@@ -2,7 +2,7 @@
 
 layout(points) in;
 layout(triangle_strip) out;
-layout(max_vertices = 4) out;
+layout(max_vertices = 14) out;
 
 uniform float pointSize;
 uniform mat4 matrix;
@@ -12,21 +12,45 @@ void main(void)
 {
     float shift = pointSize / 512.0;
 
-    gl_Position = matrix * (gl_in[0].gl_Position + vec4(-shift,-shift, 0.0, 0.0));
-    angle = 0.6;
-    EmitVertex();
+    vec4 pos1 = matrix * (gl_in[0].gl_Position + vec4(+shift,+shift,-shift, 0.0)); //1
+    vec4 pos2 = matrix * (gl_in[0].gl_Position + vec4(-shift,+shift,-shift, 0.0)); //2
+    vec4 pos3 = matrix * (gl_in[0].gl_Position + vec4(+shift,+shift,+shift, 0.0)); //3
+    vec4 pos4 = matrix * (gl_in[0].gl_Position + vec4(-shift,+shift,+shift, 0.0)); //4
+    vec4 pos5 = matrix * (gl_in[0].gl_Position + vec4(+shift,-shift,-shift, 0.0)); //5
+    vec4 pos6 = matrix * (gl_in[0].gl_Position + vec4(-shift,-shift,-shift, 0.0)); //6
+    vec4 pos7 = matrix * (gl_in[0].gl_Position + vec4(-shift,-shift,+shift, 0.0)); //7
+    vec4 pos8 = matrix * (gl_in[0].gl_Position + vec4(+shift,-shift,+shift, 0.0)); //8
 
-    gl_Position = matrix * (gl_in[0].gl_Position + vec4(-shift,+shift, 0.0, 0.0));
-    angle = 0.7;
-    EmitVertex();
+    float face_t = 1.0;
+    float face_b = 0.5;
 
-    gl_Position = matrix * (gl_in[0].gl_Position + vec4(+shift,-shift, 0.0, 0.0));
-    angle = 0.8;
-    EmitVertex();
+    float diag_f = 1.0;
+    float diag_m = 0.8;
+    float diag_b = 0.5;
 
-    gl_Position = matrix * (gl_in[0].gl_Position + vec4(+shift,+shift, 0.0, 0.0));
-    angle = 1.0;
-    EmitVertex();
+    float a1 = face_b * diag_m;
+    float a2 = face_b * diag_b;
+    float a3 = face_t * diag_m;
+    float a4 = face_t * diag_b;
+    float a5 = face_b * diag_f;
+    float a6 = face_b * diag_m;
+    float a7 = face_t * diag_m;
+    float a8 = face_t * diag_f;
+
+    gl_Position = pos4; angle = a4; EmitVertex(); // T
+    gl_Position = pos3; angle = a3; EmitVertex(); // T
+    gl_Position = pos7; angle = a7; EmitVertex(); // T
+    gl_Position = pos8; angle = a8; EmitVertex(); // T
+    gl_Position = pos5; angle = a5; EmitVertex(); // B
+    gl_Position = pos3; angle = a3; EmitVertex(); // T
+    gl_Position = pos1; angle = a1; EmitVertex(); // B
+    gl_Position = pos4; angle = a4; EmitVertex(); // T
+    gl_Position = pos2; angle = a2; EmitVertex(); // B
+    gl_Position = pos7; angle = a7; EmitVertex(); // T
+    gl_Position = pos6; angle = a6; EmitVertex(); // B
+    gl_Position = pos5; angle = a5; EmitVertex(); // B
+    gl_Position = pos2; angle = a2; EmitVertex(); // B
+    gl_Position = pos1; angle = a1; EmitVertex(); // B
 
     EndPrimitive();
 }
