c68c46d7bad9d84bba52bee3d13ee1d2749ba0d9
Adam Gagorik <adam.gagorik@gmail.com>
2014-07-28 15:26:22 -0400

    Updated sample.py to loop over parameters.

--- LangmuirPython/ga2d/sample.py
+++ LangmuirPython/ga2d/sample.py
@@ -12,6 +12,7 @@ def call(command, **kwargs):
     check_call(command.strip().split())
 
 def morph(oname, func, **kwargs):
+    image = scipy.misc.fromimage(Image.open(opts.fname).convert("L"))
     out = func(image, **kwargs)
     oname = oname.format(stub=stub, **kwargs)
     scipy.misc.imsave(oname, out)
@@ -19,28 +20,44 @@ def morph(oname, func, **kwargs):
 parser = argparse.ArgumentParser()
 parser.add_argument(dest='fname')
 parser.add_argument('--grow', action='store_true', help='run grow (slow)')
-opts = parser.parse_args()
+opts = parser.parse_args(['tf5.png'])
 
 fname = opts.fname
 stub, ext = os.path.splitext(fname)
-image = scipy.misc.fromimage(Image.open(opts.fname).convert("L"))
 
 script = './generators/block.py'
-command = 'python {script} {fname} {stub}_blocked_L0_R1 --left 0 --right 1'
+command = 'python {script} {fname} {stub}_blocked_L0_R1.png --left 0 --right 1'
 call(**locals())
 
-morph('{stub}_gblur.png', modify.gblur_and_threshold, radius=16)
+command = 'python {script} {fname} {stub}_blocked_L1_R0.png --left 1 --right 0'
+call(**locals())
 
-morph('{stub}_ublur.png', modify.ublur_and_threshold, radius=16)
+for r in [2, 4, 6, 8, 10]:
+    morph('{stub}_gblur_radius_{radius}.png', modify.gblur_and_threshold,
+          radius=r)
 
-morph('{stub}_shrink.png', modify.shrink, scale_x=0.5, scale_y=1.0)
+for r in [8, 16, 24, 32]:
+    morph('{stub}_ublur_radius_{radius}.png', modify.ublur_and_threshold,
+          radius=r)
 
-morph('{stub}_enlarge.png', modify.enlarge, zoom_x=0.75, zoom_y=0.75)
+for s in [0.25, 0.50]:
+    morph('{stub}_shrink_scale_{scale_x}_{scale_y}.png', modify.shrink,
+          scale_x=s, scale_y=s)
 
-morph('{stub}_pepper.png', modify.pepper, phase=0, count=0, percent=0.05,
-      brush='point', overlap=False, maxTries=1e9)
+for z in [1.25, 1.50]:
+    morph('{stub}_enlarge_zoom_{zoom_x}_{zoom_y}.png', modify.enlarge,
+          zoom_x=z, zoom_y=z)
 
-morph('{stub}_roughen.png', modify.roughen, fraction=0.5, dilation=0, struct=None)
+for p in [0.05, 0.10]:
+    morph('{stub}_pepper_point_percent_{percent}.png', modify.pepper, phase=0,
+          count=0, percent=p, brush='point', overlap=False, maxTries=1e9)
+
+    morph('{stub}_pepper_square_percent_{percent}.png', modify.pepper, phase=0,
+          count=0, percent=p, brush='square', overlap=False, maxTries=1e9)
+
+for f in [0.25, 0.50]:
+    morph('{stub}_roughen_fraction_{fraction}_dilation_{dilation}.png',
+          modify.roughen, fraction=f, dilation=0, struct=None)
 
 if opts.grow:
     morph('{stub}_grow.png', modify.grow_ndimage, phase=-1, pixToAdd=-1)
